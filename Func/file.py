57c654fe4599aab17fe75ba0646665f42cbc11d2
Amoh - Gyebi Godwin <amohgyebigodwin@gmail.com>
2018-02-03 16:26:40 +0000

    Add files via upload

--- Func/file.py
+++ Func/file.py
@@ -4,18 +4,33 @@ Thank you Heavenly Father
 import sys
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
-from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
+
 
 class Convertor(QObject):
     
     def __init__(self):
         QObject.__init__(self)
-
+        
+    progress = pyqtSignal(int, arguments=["start"])
+    completed = pyqtSignal(str, arguments=["completeStatus"])
+    
+    @pyqtSlot(str, str)
+    def start(self, orig_file, save_file):
+        print(orig_file[8:])
+        for x in range(2000):
+            self.progress.emit(x/10)
+        self.completeStatus(save_file)
+        
+    @pyqtSlot(str)
+    def completeStatus(self, saved_file):
+        print('here')
+        self.completed.emit(saved_file[8:])
 
 app = QGuiApplication(sys.argv)
-convertor = Convertor()
 engine = QQmlApplicationEngine()
-engine.rootContext().setContextProperty('convertor', convertor)
+convertor = Convertor()
+engine.rootContext().setContextProperty('convert', convertor)
 engine.load('main.qml')
 engine.quit.connect(app.quit)
 sys.exit(app.exec_())
